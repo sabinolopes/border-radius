89a2c8b01992c20723961d27c16c501b36ee5dd6
Sabino Lopes <sabinolopes007@gmail.com>
2023-07-25 18:58:34 -0300

    border radius generator pronto

--- src/script.js
+++ src/script.js
@@ -5,19 +5,31 @@ const bottomLeft = document.querySelector('#input-bl');
 const allBorders = document.querySelector('#input-all');
 const surface = document.querySelector('#surface');
 const textArea = document.querySelector('#text');
+const copyButton = document.querySelector('button'); 
 const borderTextTR = 'border-top-right-radius';
 const borderTextBR = 'border-bottom-right-radius';
 const borderTextTL = 'border-top-left-radius';
 const borderTextBL = 'border-bottom-left-radius';
+const borderTextAll = 'border-radius';
 
 const changeBorder = (value, border, borderText) => {
-  surface.style[border] = `${value}px`;
+  const existingText = textArea.value;
+  const regExp = new RegExp(`${borderText}: \\d+px`, 'g');
+
   if (value) {
-    textArea.value = `${borderText}: ${value}px`;
+    if (existingText.match(regExp)) {
+      textArea.value = existingText.replace(regExp, `${borderText}: ${value}px`);
+    } else {
+      textArea.value = `${existingText.trim()}\n${borderText}: ${value}px`;
+    }
+  } else {
+    textArea.value = existingText.replace(regExp, '');
   }
+
+  surface.style[border] = `${value}px`;
 };
 
-topRight.addEventListener('change', () => {
+topRight.addEventListener('input', () => {
   const borderValue = topRight.value;
   const border = 'borderTopRightRadius';
 
@@ -43,4 +55,28 @@ bottomLeft.addEventListener('input', () => {
   const border = 'borderBottomLeftRadius';
 
   changeBorder(borderValue, border, borderTextBL);
+});
+
+allBorders.addEventListener('input', () => {
+  const borderValue = allBorders.value;
+  const border = 'borderRadius';
+
+  changeBorder(borderValue, border, borderTextAll)
+});
+
+const reloadPage = () => {
+  window.location.reload();
+}
+
+
+copyButton.addEventListener('click', () => {
+  const inputText = textArea.value;
+
+  navigator.clipboard.writeText(inputText);
+
+  textArea.value = 'Copied!';
+
+  copyButton.innerHTML = `<span class="material-icons">done</span>`
+  
+  setTimeout(reloadPage, 1500);
 });
