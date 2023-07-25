b70daa7a84b8e8c112e9bc1b2346d82a31d9c9fc
Sabino Lopes <sabinolopes007@gmail.com>
2023-07-25 14:34:02 -0300

    Estrutura html e css pronta, fazendo as funções em js

---
+++ src/script.js
@@ -0,0 +1,6 @@
+const topRight = document.querySelector('input-tr');
+const bottomRight = document.querySelector('input-br');
+const topLeft = document.querySelector('input-tl');
+const bottomLeft = document.querySelector('input-bl');
+const allBorders = document.querySelector('input-all');
+const surface = document.querySelector('surface');
